86f2329353c216f1e368c5d58904d34cb7a3907d
Steffion <stefdegoey@gmail.com>
2013-05-20 12:49:05 +0200

    Added File players.yml for the new function.

--- src/me/microgeek/plugins/mvote/util/config/ConfigWrapper.java
+++ src/me/microgeek/plugins/mvote/util/config/ConfigWrapper.java
@@ -3,5 +3,6 @@ package me.microgeek.plugins.mvote.util.config;
 public class ConfigWrapper {
 
 	public static final Config VOTE_SCRIPT = new Config("config");
+	public static final Config PLAYER_DATA = new Config("players");
 	
 }
